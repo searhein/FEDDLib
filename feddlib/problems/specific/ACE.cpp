33e39400e938833423a5575bc4bc54451e3941c1
Sascha Maassen <sash@MacBook-Pro-3.fritz.box>
2021-09-08 15:12:18 +0200

    Implemented a new structure ACE using copies form the specific problem TMP

---
+++ feddlib/problems/specific/ACE.cpp
@@ -0,0 +1,7 @@
+#include "ACE_decl.hpp"
+#ifdef HAVE_EXPLICIT_INSTANTIATION
+#include "ACE_def.hpp"
+namespace FEDD {
+    template class ACE<default_sc, default_lo, default_go, default_no>;
+}
+#endif
---
+++ feddlib/problems/specific/ACE.hpp
@@ -0,0 +1,10 @@
+#ifndef ACE_hpp
+#define ACE_hpp
+
+#include "ACE_decl.hpp"
+
+//#ifndef HAVE_EXPLICIT_INSTANTIATION
+//#include "ACE_def.hpp"
+//#endif
+
+#endif
