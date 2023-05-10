307675f383a7d367800660ca86fba58cd0caf801
Alexander Heinlein <a.heinlein@tudelft.nl>
2023-05-10 09:54:53 +0200

    Adding pybind11 as TPL.

--- TPLsList.cmake
+++ TPLsList.cmake
@@ -1,5 +1,6 @@
 TRIBITS_REPOSITORY_DEFINE_TPLS(
     MPI             "${${PROJECT_NAME}_TRIBITS_DIR}/core/std_tpls/"     PT
+    pybind11        "cmake/TPLs/"                                       ST
     Trilinos        "cmake/TPLs/"                                       PT
 )
 
